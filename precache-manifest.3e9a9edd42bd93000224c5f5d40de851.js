20e8adb57dc721fec05922af36cac0c28817e5da
Sneha Herle <sneha.herle@gmail.com>
2020-10-23 12:05:59 +0530

    Updates

--- precache-manifest.3e9a9edd42bd93000224c5f5d40de851.js
+++ precache-manifest.3e9a9edd42bd93000224c5f5d40de851.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "4b77f5a79bd46d5f7c3311c79bf97a7f",
+    "revision": "a37011946a38232cd7da6e6d2e0af4c7",
     "url": "/home/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/home/static/css/2.5b44f0a0.chunk.css"
   },
   {
-    "revision": "53df9a059e7dd66826dd",
+    "revision": "e84617de9d929bc05630",
     "url": "/home/static/css/main.653f69f3.chunk.css"
   },
   {
@@ -20,19 +20,19 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/home/static/js/2.cb55fa26.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "53df9a059e7dd66826dd",
-    "url": "/home/static/js/main.ef9f0b0e.chunk.js"
+    "revision": "e84617de9d929bc05630",
+    "url": "/home/static/js/main.111cd1c7.chunk.js"
   },
   {
     "revision": "c0578b487099793a068c",
     "url": "/home/static/js/runtime-main.3c590061.js"
   },
   {
-    "revision": "bd08aa0f3e9e1c5c3377bb73a85bdcc5",
-    "url": "/home/static/media/Ana Ashrafi - Resume 2020.bd08aa0f.pdf"
-  },
-  {
     "revision": "16124a708828582346b5e1db9b8c1fc6",
     "url": "/home/static/media/Profile.16124a70.jpg"
+  },
+  {
+    "revision": "e1978abb9a38803e4bd54fdeab4ab16a",
+    "url": "/home/static/media/Sneha Herle.e1978abb.pdf"
   }
 ]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/home/precache-manifest.3c224b3fd3006bbc401fa987c16ec9f0.js"
+  "/home/precache-manifest.3e9a9edd42bd93000224c5f5d40de851.js"
 );
 
 self.addEventListener('message', (event) => {
