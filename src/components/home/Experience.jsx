5cc3ea6198bbd057dcd7d0d833b9669ca9882043
Sneha Herle <sneha.herle@gmail.com>
2021-01-01 17:04:58 +0530

    edit experience

--- src/components/home/Experience.jsx
+++ src/components/home/Experience.jsx
@@ -27,18 +27,18 @@ return (
                     <p className="lead text-center" style={{fontSize:'22px'}}>
                         Software Developer 
                         <br/>
-                        September 2018 - Present
+                        September 2018 - December 2020
                     </p>
                     <p className="lead text-center" style={{fontSize:'22px', fontStyle:'italic', color:'#1976d2'}}>
                     Received the SPOT Award for FY'21 Q1 for outstanding work as a Developer for 2 projects - Amedisys and NBS.
                     </p>
-                    {/*
+                    {
                     <p className="lead text-center" style={{fontSize:'22px', fontStyle:'italic'}}>
                     "Sneha has been key member on NBS and Amedisys project, she has contributed to fixing critical defects and fronting customer, she has been interacting with them and has gained customer confidence. Sneha has worked on many of the S1 and S2 tickets. She demonstrated excellent functional and technical understanding of the projects she is working. This nomination is mainly for her excellent job done in ensuring quality, minimal rework and delivering on time and quality release on the fix related to critical projects like NBS and Amedisys."
                     <br/>
                     ~ Ramesh Surabathula (Technical Manager @MetricStream)
                     </p>
-                    */}
+                    }
                     {/* <h3>Dell Technologies</h3> */}
                 </div>
             </div>
